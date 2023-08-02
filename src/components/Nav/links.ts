f8d1f70b7c0edace2b0d41eeb9fba797bc66bf18
Brett Bass <bbass@ucsd.edu>
2023-08-02 12:52:30 -0700

    converted links.json to links.ts file

---
+++ src/components/Nav/links.ts
@@ -0,0 +1,34 @@
+// Define the type for a single link
+export type Link = {
+    label: string;
+    href: string;
+    download?: boolean;
+};
+
+// Export the array of links
+const links: Link[] = [
+    {
+        label: 'Home',
+        href: '',
+    },
+    {
+        label: 'About',
+        href: '/about',
+    },
+    {
+        label: 'Projects',
+        href: '/projects',
+    },
+    {
+        label: 'Contact',
+        href: '/contact',
+    },
+    {
+        label: 'Resume',
+        href:
+            'https://drive.google.com/u/0/uc?id=1STGoP0NsWAKwwotpKrYErdOFTQEae44u&export=download',
+        download: true,
+    },
+];
+
+export default links;
